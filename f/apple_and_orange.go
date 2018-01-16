e1ff599ad5275e6b813f3b33801c07d1cc911673
fooxlj07 <jing.li.frch@outlook.com>
2018-01-16 22:03:08 +0800

    apple and orange

---
+++ f/apple_and_orange.go
@@ -0,0 +1,33 @@
+package f
+
+import (
+	"fmt"
+
+	"github.com/hacker_rank/utils"
+)
+
+func AppleAndOrange() {
+
+	rangeLimit := utils.ReadIntsInput(2)
+
+	positions := utils.ReadIntsInput(2)
+	line3 := utils.ReadIntsInput(2)
+	appleNum := line3[0]
+	orangeNum := line3[1]
+
+	appleDistances := utils.ReadIntsInput(appleNum)
+	orangeDistances := utils.ReadIntsInput(orangeNum)
+	fmt.Println(caculateFruit(positions[0], rangeLimit, appleDistances))
+	fmt.Println(caculateFruit(positions[1], rangeLimit, orangeDistances))
+}
+
+func caculateFruit(pos int, rangeLimit, allDistaces []int) int {
+	sum := 0
+	for _, d := range allDistaces {
+		currentPos := pos + d
+		if currentPos >= rangeLimit[0] && currentPos <= rangeLimit[1] {
+			sum = sum + 1
+		}
+	}
+	return sum
+}
---
+++ utils/read_utils.go
@@ -0,0 +1,19 @@
+package utils
+
+//This file is utils which read input from console
+
+import (
+	"fmt"
+)
+
+//ReadIntsInput ...
+//Read several lines of int,each line contain just one int
+//first line idicate how many records it will receive
+func ReadIntsInput(num int) []int {
+	inputs := make([]int, num)
+
+	for i := range inputs { // break the loop if text == "q"
+		_, _ = fmt.Scanf("%d", &inputs[i])
+	}
+	return inputs
+}
